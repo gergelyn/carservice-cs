70d270af702f20899ed510b548f62255c00cf1cb
gergelyn <preszthegas@gmail.com>
2022-01-03 16:12:28 +0100

    ClientContext coded

---
+++ CarService_Server/Repositories/ClientContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using CarService_Server.Models;
+
+namespace CarService_Server.Repositories
+{
+    public class ClientContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ClientContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
