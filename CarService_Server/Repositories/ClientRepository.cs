acd8fa2b3316bbccbb9a464db5acd950ef8103c9
gergelyn <preszthegas@gmail.com>
2022-01-04 15:15:24 +0100

    ClientRepository init

---
+++ CarService_Server/Repositories/ClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CarService_Server.Models;
+
+namespace CarService_Server.Repositories
+{
+    public class ClientRepository
+    {
+    }
+}
