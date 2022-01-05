cd145300728493b85af4b26e75d0261a6aa4de3f
gergelyn <preszthegas@gmail.com>
2022-01-05 14:38:34 +0100

    Client model

---
+++ CarService_AdminClient/Data/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CarService_AdminClient.Data
+{
+    public class Client
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CarType { get; set; }
+
+        public string LicensePlate { get; set; }
+
+        public string Issue { get; set; }
+
+        public string Progress { get; set; }
+    }
+}
