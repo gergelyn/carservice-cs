f7fe6ca385ff78103ca55bce506993a7900e8259
gergelyn <preszthegas@gmail.com>
2022-01-09 17:10:03 +0100

    AddClient service

--- CarService_AdminClient/Pages/AddClient.cs
+++ CarService_AdminClient/Pages/AddClient.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using CarService_AdminClient.Data;
+
 namespace CarService_AdminClient.Pages
 {
-    public class AddClient
+    public partial class AddClient
     {
-        public AddClient()
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public Client Client { get; set; } = new Client();
+
+        private async Task SubmitForm()
         {
+            await HttpClient.PostAsJsonAsync("client", Client);
+            NavigationManager.NavigateTo("clients");
         }
     }
 }
