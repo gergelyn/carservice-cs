baff1ebbfee8159eab847407c5e4a4c2827afbaa
gergelyn <preszthegas@gmail.com>
2022-01-09 13:38:31 +0100

    Scoping Web API

---
+++ CarService_AdminClient/Pages/ClientList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CarService_AdminClient.Pages
+{
+    public class ClientList
+    {
+        public ClientList()
+        {
+        }
+    }
+}
--- CarService_AdminClient/Startup.cs
+++ CarService_AdminClient/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CarService_AdminClient.Data;
+using System.Net.Http;
 
 namespace CarService_AdminClient
 {
@@ -28,6 +29,7 @@ namespace CarService_AdminClient
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5002/api/") });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
