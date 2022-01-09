cce7694ad21c217bec30fcf3e3deb7940257ed2f
gergelyn <preszthegas@gmail.com>
2022-01-09 18:05:23 +0100

    ClientEdit services

--- CarService_AdminClient/Pages/ClientEdit.cs
+++ CarService_AdminClient/Pages/ClientEdit.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using CarService_AdminClient.Data;
+
+
 namespace CarService_AdminClient.Pages
 {
-    public class ClientEdit
+    public partial class ClientEdit
     {
-        public ClientEdit()
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public Client Client { get; set; }
+
+        [Parameter]
+        public string ClientID { get; set; }
+
+        protected override async Task OnInitializedAsync()
         {
+            Client = await HttpClient.GetFromJsonAsync<Client>($"client/{ClientID}");
+            await base.OnInitializedAsync();
+        }
+
+        void NavigateBack()
+        {
+            NavigationManager.NavigateTo("clients");
+        }
+
+        private async Task SubmitForm()
+        {
+            await HttpClient.PutAsJsonAsync($"client/{ClientID}", Client);
+            NavigationManager.NavigateTo($"clients/{ClientID}");
         }
     }
 }
