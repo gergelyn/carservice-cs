1549fbdfe0391cd18814ff5b1ff9bc83f6d879a4
gergelyn <preszthegas@gmail.com>
2022-01-09 17:42:12 +0100

    ClientDetails services

--- CarService_AdminClient/Pages/ClientDetails.cs
+++ CarService_AdminClient/Pages/ClientDetails.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using CarService_AdminClient.Data;
+
 namespace CarService_AdminClient.Pages
 {
-    public class ClientDetails
+    public partial class ClientDetails
     {
-        public ClientDetails()
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public Client Client { get; set; }
+
+        [Parameter]
+        public string ClientID { get; set; }
+
+        protected override async Task OnInitializedAsync()
         {
+            Client = await HttpClient.GetFromJsonAsync<Client>($"client/{ClientID}");
+            await base.OnInitializedAsync();
+        }
+
+        void NavigateBack()
+        {
+            NavigationManager.NavigateTo("clients");
+        }
+
+        private async Task DeleteClient()
+        {
+            await HttpClient.DeleteAsync($"client/{ClientID}");
+            NavigationManager.NavigateTo("clients");
         }
     }
 }
