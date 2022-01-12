685ead71b056c34e82606b7777dbac0edc135ee6
gergelyn <preszthegas@gmail.com>
2022-01-12 12:11:54 +0100

    ProgressForm code-behind

---
+++ CarService_MechanicClient/Shared/ProgressForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using CarService_MechanicClient.Data;
+
+namespace CarService_MechanicClient.Shared
+{
+    public partial class ProgressForm
+    {
+        [Parameter]
+        public Client Client { get; set; }
+
+        [Parameter]
+        public Func<Task> SubmitForm { get; set; }
+    }
+}
