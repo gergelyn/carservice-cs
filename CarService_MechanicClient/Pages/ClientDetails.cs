c6f3cb632e637b131dcaee6824d9fc1dea490565
gergelyn <preszthegas@gmail.com>
2022-01-12 14:03:13 +0100

    Import model from common library for mechanic client

--- CarService_MechanicClient/Pages/ClientDetails.cs
+++ CarService_MechanicClient/Pages/ClientDetails.cs
@@ -2,7 +2,7 @@
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-using CarService_MechanicClient.Data;
+using CarService_Common.Models;
 
 namespace CarService_MechanicClient.Pages
 {
--- CarService_MechanicClient/Startup.cs
+++ CarService_MechanicClient/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CarService_MechanicClient.Data;
+using CarService_Common.Models;
 using System.Net.Http;
 
 namespace CarService_MechanicClient
