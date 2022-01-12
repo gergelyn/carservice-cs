f62bf636ca2f54906f49da3e566ac37a94b1534f
gergelyn <preszthegas@gmail.com>
2022-01-12 12:11:07 +0100

    ClientList code-behind

---
+++ CarService_MechanicClient/Pages/ClientList.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using CarService_MechanicClient.Data;
+
+
+namespace CarService_MechanicClient.Pages
+{
+    public partial class ClientList : ComponentBase
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public Client[] Clients { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Clients = await HttpClient.GetFromJsonAsync<Client[]>("client");
+            await base.OnInitializedAsync();
+        }
+    }
+}
