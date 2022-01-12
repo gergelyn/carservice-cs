531da4ef1e50ab9ceeb64c4e01cbcd12fdd14277
gergelyn <preszthegas@gmail.com>
2022-01-12 14:03:42 +0100

    MSTest project init

---
+++ CarService.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
